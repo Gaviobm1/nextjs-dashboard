a93b1755614e4af5fd1158517aa95f3ef2d6dbff
Gavin O Brien <142741214+Gaviobm1@users.noreply.github.com>
2024-08-24 12:32:55 +0200

    initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana, New_Rocker } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const lusitana = Lusitana({
+  subsets: ["latin"],
+  weight: ["700", "400"],
+});
+export const newRocker = New_Rocker({ subsets: ["latin"], weight: ["400"] });
